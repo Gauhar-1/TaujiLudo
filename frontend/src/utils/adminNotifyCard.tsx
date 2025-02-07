bc855c4c5430730d446846fde9482eb11ff79a18
Gauhar-1 <mustakkhan1210@gmail.com>
2025-02-07 18:21:31 +0530

    markAsRead  notification frontend

--- frontend/src/utils/adminNotifyCard.tsx
+++ frontend/src/utils/adminNotifyCard.tsx
@@ -1,4 +1,24 @@
-export const NotifyCard = ()=>{
+// import { formatDistanceToNowStrict } from "date-fns";
+import { API_URL } from "./url";
+import axios from "axios";
+
+export const NotifyCard = (props : any)=>{
+
+    const handleRead = async()=>{
+        
+        const id = props.notification._id;
+        if(!id){
+            return console.log("notification id not found!");
+        }
+
+        const response = await axios.post(`${API_URL}/api/auth/markRead`, { id });
+
+        if(!response.data){
+            return console.log("response not found");
+        }
+
+    }
+
     return (
         <div className="p-4">
            { <div className=" border border-black bg-gray-400 rounded-lg flex justify-between p-3">
@@ -9,24 +29,27 @@ export const NotifyCard = ()=>{
                 <div className="px-4 font-serif text-lg  ">
                     <div className="flex gap-2">
                     <div className="text-white">Manish</div>
-                    {/* <div className="text-green-300 pt-1">has joined!!</div> */}
-                    <div className="text-green-300">has deposited!!</div>
+                    {/* {props.notifications.type <div className="text-green-300 pt-1">has joined!!</div>} */}
+                    <div className={` ${props.notification.type === "deposit" ? "text-green-400": "text-red-500"} `}>has {props.notification.type === "deposit" ? "deposited": "withdrawed"}!!</div>
                     </div>
                     <div className="flex gap-1">
                         <img src="/money.png" alt="" className="h-5"/>
-                        <div className="text-sm ">(+)</div>
-                        <div className="text-sm font-mono">500</div>
+                        <div className={`text-sm ${props.notification.type === "deposit" ? "text-green-400": "text-red-500"} `}>{props.notification.type === "deposit" ? "(+)": "(-)"}</div>
+                        <div className="text-sm font-mono">{props.notification.amount}</div>
                     </div>
                 </div>
             </div>
                 <div className="flex  flex-col gap-2">
-                    <div className="flex justify-center ">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6" >
-  <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                    <div className="flex justify-center " onClick={()=>{
+                        handleRead();
+                    }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill={`${props.notification.
+markAsRead ? "gray" : "yellow"}`} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6" >
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
 </svg>
 
                     </div>
-<div className="text-xs">5 hour ago</div>
+{/* <div className="">{formatDistanceToNowStrict(new Date(props.notification.createdAt), { addSuffix: true })}</div> */}
 
                 </div>
 
--- frontend/src/utils/notifyCard.tsx
+++ frontend/src/utils/notifyCard.tsx
@@ -1,7 +1,14 @@
-export const NotifyCard = ()=>{
+import {  formatDistanceToNowStrict } from "date-fns"
+
+export const NotifyCard = ( props : any)=>{
     return (
-        <div className="mx-6 mb-2  bg-red-300 p-4 rounded-md border border-black shadow-md">
-            <div>hii</div>
+        <div className="mx-6 mb-2  bg-red-300 p-4 rounded-md border border-black shadow-md flex flex-col">
+            <div className=" w-full overflow-auto break-words">{props.notification.reason}</div>
+            <div className="flex justify-end text-gray-500">
+            <div>
+            {formatDistanceToNowStrict(new Date(props.notification.createdAt), { addSuffix: true })}
+    </div>
+            </div>
         </div>
     )
 }
