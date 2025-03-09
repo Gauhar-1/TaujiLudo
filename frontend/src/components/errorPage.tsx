b9b31008546803b2304496b6b813bf1595a3db0c
Gauhar-1 <mustakkhan1210@gmail.com>
2025-03-09 15:38:44 +0530

    last day

--- frontend/src/components/errorPage.tsx
+++ frontend/src/components/errorPage.tsx
@@ -1,7 +1,7 @@
 export const ErrorPage = ()=>{
     return (
         <div className='bg-white border border-gray-300 max-w-sm min-h-screen text-center'>
-            <img src="logo.png" alt="" />
+            <img src="/logo.png" alt="" />
       <h1 className='font-bold text-xl'>We're experiencing issues right now!</h1>
       <p className='font-semibold'>Please try again in a few moments.</p>
     </div>
