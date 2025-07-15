e3f7aad25a521f652fa8caedd8c507be48975681
Gauhar-1 <mustakkhan1210@gmail.com>
2025-07-15 11:12:22 +0530

    Maintainance 4

--- frontend/src/utils/url.tsx
+++ frontend/src/utils/url.tsx
@@ -1,4 +1,4 @@
 // Get API_URL based on the environment
-export const API_URL =  "https://taujiludo.onrender.com:3000";
+export const API_URL =  "https://taujiludo.onrender.com";
 // export const API_URL =  "http://localhost:3000";
 export const SOCKET_URL = "https://taujiludo.onrender.com:5000";
